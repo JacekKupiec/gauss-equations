f1fef017f7f745f8b0884da6ae5a1b577432b32e
JacekKupiec <jacek.kupiec@o2.pl>
2018-01-15 18:08:28 +0100

    Poprawa skryptu do odpalania
    Dodałem możliwość łatwej zmiany rozmiaru macierzy poprzez mienne ROWS i COLUMNS oraz ile razy maja sie wykonac każda z pątli.

--- run_small.sh
+++ run_small.sh
@@ -1,67 +1,72 @@
+ROW_SIZE=3000
+COLUMN_SIZE=3001
+LOOP_COUNT=20
+
 echo basic
-for ((i=1;i<=20;i++)); 
+for ((i=1;i<=$LOOP_COUNT;i++)); 
 do 
-   ./basic
+   ./basic r p $ROW_SIZE $COLUMN_SIZE
 done
 
 echo basic + def_vec
-for ((i=1;i<=20;i++)); 
+for ((i=1;i<=$LOOP_COUNT;i++)); 
 do 
-   ./basic_def_vec
-done
-
-echo optimal - def_vec
-for ((i=1;i<=20;i++)); 
-do 
-   ./optimal_def_vec
+   ./basic_def_vec r p $ROW_SIZE $COLUMN_SIZE
 done
 
 echo basic + man_vec
-for ((i=1;i<=20;i++)); 
+for ((i=1;i<=$LOOP_COUNT;i++)); 
 do 
-   ./basic_man_vec
-done
-
-echo opitmal - man_vec
-for ((i=1;i<=20;i++)); 
-do 
-   ./opitmal_man_vec
+   ./basic_man_vec r p $ROW_SIZE $COLUMN_SIZE
 done
 
 echo basic + fsp
-for ((i=1;i<=20;i++)); 
+for ((i=1;i<=$LOOP_COUNT;i++)); 
 do 
-   ./basic_fsp
-done
-
-echo optimal - fsp
-for ((i=1;i<=20;i++)); 
-do 
-   ./optimal_fsp
+   ./basic_fsp r p $ROW_SIZE $COLUMN_SIZE
 done
 
 echo optimal - aff
-for ((i=1;i<=20;i++)); 
+for ((i=1;i<=$LOOP_COUNT;i++)); 
 do 
-   ./optimal
+   ./optimal r p $ROW_SIZE $COLUMN_SIZE
+done
+
+export OMP_NUM_THREADS=116
+export KMP_AFFINITY=scatter
+
+echo basic + affinity
+for ((i=1;i<=$LOOP_COUNT;i++)); 
+do 
+   ./basic r p $ROW_SIZE $COLUMN_SIZE
+done
+
+echo optimal - def_vec
+for ((i=1;i<=$LOOP_COUNT;i++)); 
+do 
+   ./optimal_def_vec r p $ROW_SIZE $COLUMN_SIZE
+done
+
+echo opitmal - man_vec
+for ((i=1;i<=$LOOP_COUNT;i++)); 
+do 
+   ./optimal_man_vec r p $ROW_SIZE $COLUMN_SIZE
+done
+
+echo optimal - fsp
+for ((i=1;i<=$LOOP_COUNT;i++)); 
+do 
+   ./optimal_fsp r p $ROW_SIZE $COLUMN_SIZE
 done
 
 echo optimal
-export OMP_NUM_THREADS=116
-export KMP_AFFINITY=scatter
-for ((i=1;i<=20;i++)); 
+for ((i=1;i<=$LOOP_COUNT;i++)); 
 do 
-   ./optimal
-done
-
-echo basic + affinity
-for ((i=1;i<=20;i++)); 
-do 
-   ./basic
+   ./optimal r p $ROW_SIZE $COLUMN_SIZE
 done
 
 echo sequential
-for ((i=1;i<=20;i++)); 
+for ((i=1;i<=$LOOP_COUNT;i++)); 
 do 
-   ./sequential
+   ./sequential r p $ROW_SIZE $COLUMN_SIZE
 done
